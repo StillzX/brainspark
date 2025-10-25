764beb6beaa830d8efc511a6891bb4a30d92b240
Miguel C. Gonçalves <studio.dev2909@gmail.com>
2025-10-25 15:27:17 -0400

    Adicionado interface básica e app.js para enviar dados a API e atualizar a interface.

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'];
-$db   = $_ENV['DB_NAME'];
+$db = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
 $charset = 'utf8mb4';
@@ -19,23 +18,27 @@ $options = [
 ];
 
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
+    $pdo = new PDO($dsn, $user, $pass, $options);
 } catch (\PDOException $e) {
-     http_response_code(500);
-     echo json_encode(['status' => 'erro', 'mensagem' => 'Erro interno do servidor.']);
-     exit;
+    http_response_code(500);
+    echo json_encode(['status' => 'erro', 'mensagem' => 'Erro interno do servidor.']);
+    exit;
 }
 
-$code_room_user = $_POST['room_code'] ?? '';
-$username = $_POST['username'] ?? '';
+$code_room_user = $_POST['codigo_sala'] ?? '';
+$username = $_POST['nome_jogador'] ?? '';
 
 if (empty($code_room_user) || empty($username)) {
-    echo json_encode(['status' => 'erro', 'mensagem' => 'Nome e código da sala são obrigatórios.']);
+    echo json_encode([
+    'status' => 'erro',
+    'mensagem' => 'Nome e código da sala são obrigatórios.'
+    ]);
+    
     exit;
 }
 
 
-function searchRoom($pdo, $code) {
+function searchRoom($pdo, $code){
     $sql = "SELECT * FROM rooms WHERE room_code = ? LIMIT 1";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$code]);
@@ -43,7 +46,7 @@ function searchRoom($pdo, $code) {
 }
 
 
-function joinRoom($pdo, $id_room, $username) {
+function joinRoom($pdo, $id_room, $username){
     $sql = "INSERT INTO players (id_room, player_name, player_pontuation) VALUES (?, ?, 0)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id_room, $username]);
@@ -51,7 +54,8 @@ function joinRoom($pdo, $id_room, $username) {
     return $pdo->lastInsertId(); // Retorna o ID do jogador recém-criado
 }
 
-function createRoomAndJoin($pdo, $code, $username) {
+// Reutiliza a função 'joinRandom' para entrar na sala após criar essa sala
+function createRoomAndJoin($pdo, $code, $username){
     $sql_sala = "INSERT INTO rooms (room_code, room_status) VALUES (?, 'waiting')";
     $stmt_sala = $pdo->prepare($sql_sala);
     $stmt_sala->execute([$code]);
@@ -63,7 +67,7 @@ function createRoomAndJoin($pdo, $code, $username) {
 }
 
 
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=utf-8');
 $resposta_json = [];
 
 $salaEncontrada = searchRoom($pdo, $code_room_user);
@@ -71,33 +75,29 @@ $salaEncontrada = searchRoom($pdo, $code_room_user);
 if ($salaEncontrada) {
     if ($salaEncontrada['room_status'] == 'waiting') {
         $jogador_id = joinRoom($pdo, $salaEncontrada['id_room'], $username);
-        
+
         $resposta_json = [
-            'status' => 'sucesso', 
-            'acao' => 'entrou', 
-            'sala_id' => $salaEncontrada['id_room'], 
+            'status' => 'sucesso',
+            'acao' => 'entrou',
+            'sala_id' => $salaEncontrada['id_room'],
             'jogador_id' => $jogador_id
         ];
-        
     } else {
         $resposta_json = [
-            'status' => 'erro', 
+            'status' => 'erro',
             'mensagem' => 'Essa sala não está aceitando novos jogadores.'
         ];
     }
-
 } else {
     // retorna o id da sala criada e o id do primeiro jogador (você)
     $info = createRoomAndJoin($pdo, $code_room_user, $username);
-    
+
     $resposta_json = [
-        'status' => 'sucesso', 
-        'acao' => 'criou', 
-        'sala_id' => $info['sala_id'], 
+        'status' => 'sucesso',
+        'acao' => 'criou',
+        'sala_id' => $info['sala_id'],
         'jogador_id' => $info['jogador_id']
     ];
 }
 
 echo json_encode($resposta_json);
-
-?>
